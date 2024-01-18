edb266d22fe96314859181ffe4b7959e4f4b2d69
MatteBlackIT <bouwkamp25@gmail.com>
2024-01-17 23:18:20 -0600

    Strings always have their Methods.

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -2,3 +2,17 @@ let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
+
+//slice
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part);
+
+//replace
+let language = "JavaScript";
+language.replace('J', 'Q');
+language.slice(0,5);
+console.log(language);
