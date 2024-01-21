b889195eef8913c6f5b5cd327ffd5794d144c215
MatteBlackIT <bouwkamp25@gmail.com>
2024-01-21 16:28:21 -0600

    Completed Practice w/ loops

--- loops/chapter-examples/Reversing-a-String.js
+++ loops/chapter-examples/Reversing-a-String.js
@@ -1,8 +1,19 @@
-let str = "blue";
-let reversed = "";
+let str = "Supercalifragilisticexpialidocious";
+let rev = "";
 
 for (let i = 0; i < str.length; i++) {
-    reversed = str[i] + reversed;
+    rev = str[i] + rev;
 }
 
-console.log(reversed);
+console.log(rev);
+    console.log(rev.length);
+
+let quote = "namesoB kciwdahC- !detfig dna ,kcalb ,gnuoy eb oT";
+let reverse = "";
+
+    for (let i = 0; i < quote.length; i++) {
+        reverse = quote[i] + reverse
+    }
+
+console.log(reverse);
+    console.log("Wakanda Forever!");
--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
-
+let names = ["Matte", "Manda", "Challa", "Appa"]
 // write a for loop that prints each name on a different line
+
+for (i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
