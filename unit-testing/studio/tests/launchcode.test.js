3bc1ea2fb0c5ff292919da140dc1b6e00b35f03e
MatteBlackIT <bouwkamp25@gmail.com>
2024-02-08 20:58:30 -0600

    Testing 123

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,37 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
-  
+  test("return 'nonprofit' as key value of organization", function(){
+    let keyval = launchcode.organization;
+    expect(keyval).toBe('nonprofit');
+  })
+
+  test('returns "X!" when divisible by Y', function(){
+    expect(launchcode.launchOutput(2)).toBe('Launch!');
+    expect(launchcode.launchOutput(3)).toBe('Code!');
+    expect(launchcode.launchOutput(5)).toBe('Rocks!');
+  })
+
+  test('return "Jeff" as key value of executiveDirector', function() {
+    let keyVal = launchcode.executiveDirector;
+    expect(keyVal).toBe("Jeff")
+  })
+
+  test('return "100%" as key value of percentageCoolEmployees', function() {
+    let keyVal = launchcode.percentageCoolEmployees;
+    expect(keyVal).toBe(100)
+  })
+
+  test('return "[val1, val2, val3]" as key value of programsOffered', function(){
+    let keyVal = launchcode.programsOffered;
+    let index = launchcode.programsOffered.length;
+    expect(keyVal[0]).toBe("Web Development")
+    expect(keyVal[1]).toBe("Data Analysis")
+    expect(keyVal[2]).toBe("Liftoff")
+    expect(index).toBe(3)
+  })
+
+  test('returns "X!" when divisible by Y', function(){
+    expect(launchcode.launchOutput(2 && 3)).toBe('LaunchCode!')
+  })
 });
